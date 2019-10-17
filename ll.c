f0fa4307aee5c2f182ebb0d7f6fc2331442d42e2
Aman Aggarwal <54923729+Aman123010@users.noreply.github.com>
2019-10-17 20:10:58 +0530

    Add files via upload

---
+++ ll.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<stdlib.h>
+int data;
+void display();
+struct node
+{
+int info;
+struct node *next;
+}*start=NULL;
+
+void add_beg()
+{
+printf("\nEnter element to add ");
+scanf("%d",&data);
+struct node *newnode;
+newnode=(struct node*)malloc(sizeof(struct node));
+newnode->info=data;
+newnode->next=start;
+start=newnode;
+display();
+}
+void add_end()
+{
+printf("\nEnter element to add ");
+scanf("%d",&data);
+struct node *temp;
+struct node *newnode;
+newnode=(struct node*)malloc(sizeof(struct node));
+newnode->info=data;
+newnode->next=NULL;
+temp=start;
+while(temp->next!=NULL)
+{
+temp=temp->next;
+}
+temp->next=newnode;
+display();
+
+}
+void del_beg()
+{
+struct node *temp;
+temp=start;
+start=start->next;
+free(temp);
+display();
+}
+void del_end()
+{
+struct node *temp,*ptr;
+temp=start;
+while(temp->next->next!=NULL)
+temp=temp->next;
+ptr=temp->next;
+temp->next=NULL;
+free(ptr);
+display();
+}
+void display()
+{
+struct node *temp;
+temp=start;
+if(start==NULL)
+printf("list is empty");
+else
+while(temp!=NULL)
+{
+printf("\ndata=%d\tpresent add=%u\tnext add=%u\n",temp->info,temp,temp->next);
+temp=temp->next;
+}
+}
+void search()
+{
+int key;
+printf("Element to be searched");
+scanf("%d",&key);
+struct node *temp;
+temp=start;
+if(start==NULL)
+printf("list is empty");
+else
+while(temp->info!=key)
+{
+temp=temp->next;
+}
+printf("Element found at present add=%u",temp);
+}
+int main()
+{
+int a;
+while(1)
+{
+printf("\n1.Add at beginning\n2.Add at end\n3.Delete from beginning\n4.Delete from end\n5.Search\nEnter choice ");
+scanf("%d",&a);
+switch(a)
+{
+case 1:add_beg();
+break;
+case 2:add_end();
+break;
+case 3:del_beg();
+break;
+case 4:del_end();
+break;
+case 5:search();
+break;
+}
+}
+}
+
+
